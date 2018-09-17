caf54d88f69a8da91ec8a2ef7af6811d02074580
Nyalls Hemingway <nyallshemingway@Nyallss-MacBook-Pro.local>
2018-09-17 14:40:49 +0100

    second test passed

--- cohort_class/spec/test_cohort_class.rb
+++ cohort_class/spec/test_cohort_class.rb
@@ -4,8 +4,23 @@ require_relative('../cohort_class.rb')
 
 
 class TestCohortClass < MiniTest::Test
+
+
   def test_student_name
     student_name = CohortClass.new("Jen", "E25")
     assert_equal("Jen", student_name.name)
   end
+
+  def test_student_cohort
+    student_cohort = CohortClass.new("Jackie", "E25")
+    assert_equal("E25", student_cohort.cohort)
+  end 
+
+
+
+
+
+
+
+
 end
